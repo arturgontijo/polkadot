3daed4e0780c4c150f60a2be535230f728d43e3d
Artur Gontijo <arturgontijo@gmail.com>
2022-10-21 20:09:56 -0300

    [XCMv3] Rococo with FixedWeightBounds.

--- xcm/pallet-xcm-benchmarks/src/fungible/mock.rs
+++ xcm/pallet-xcm-benchmarks/src/fungible/mock.rs
@@ -51,9 +51,7 @@ frame_support::construct_runtime!(
 parameter_types! {
 	pub const BlockHashCount: u64 = 250;
 	pub BlockWeights: frame_system::limits::BlockWeights =
-		frame_system::limits::BlockWeights::simple_max(
-			Weight::from_ref_time(1024).set_proof_size(u64::MAX),
-		);
+		frame_system::limits::BlockWeights::simple_max(Weight::from_ref_time(1024));
 }
 impl frame_system::Config for Test {
 	type BaseCallFilter = Everything;
